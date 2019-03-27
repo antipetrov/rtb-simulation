bcbede89612ce6799ad257ec1be355f1f898c0cb
Petr Matukhov <petr.matukhov@gmail.com>
2019-03-27 16:41:37 +0300

    basic list

--- ad_buy/ad_app/models.py
+++ ad_buy/ad_app/models.py
@@ -4,10 +4,26 @@ from django.db import models
 
 
 class Ad(models.Model):
-    content=models.TextField()
-    categories = models.ManyToManyField("Category", related_name="ads")
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    content = models.TextField()
+    categories = models.ManyToManyField("Category", related_name="ads", verbose_name="Категории", )
+
+    def __str__(self):
+        return "Объявление #{}: {}".format(self.pk, self.content[:32])
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
     name = models.CharField(verbose_name="Название", max_length=255)
     tag = models.CharField(verbose_name="Тег", max_length=255)
+
+    def __str__(self):
+        return "Категория #{}: {}".format(self.pk, self.name)
--- ad_buy/ad_app/views.py
+++ ad_buy/ad_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . models import Ad, Category
 
 def index(request):
-    return render(request, 'index.html')
+
+    ads = Ad.objects.all()
+
+    return render(request, 'index.html', {'ads': ads})
