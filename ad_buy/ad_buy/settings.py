90ba51d343671d306ba422b6fe9d2be9f5429271
Petr Matukhov <petr.matukhov@gmail.com>
2019-03-27 17:12:43 +0300

    migrations & static

--- ad_buy/ad_buy/settings.py
+++ ad_buy/ad_buy/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -51,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ad_app',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
