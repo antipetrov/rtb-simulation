fef3cd45afe68e37866aace2582190138975f820
Petr Matukhov <petr.matukhov@gmail.com>
2019-03-28 16:31:30 +0300

    ad list ui

--- ad_buy/ad_app/views.py
+++ ad_buy/ad_app/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.conf import settings
+
 from . models import Ad, Category
+
 
 def index(request):
 
-    ads = Ad.objects.all()
+    cat = request.GET.get('cat')
+    search = request.GET.get('search')
+    page = request.GET.get('page', 1)
 
-    return render(request, 'index.html', {'ads': ads})
+    ads = Ad.objects
+
+    category_selected = None
+    if cat:
+        category_selected = Category.objects.filter(tag=cat).first()
+        if category_selected:
+            ads = ads.filter(categories__pk=category_selected.pk)
+
+    if search:
+        ads = ads.filter(content__icontains=search)
+
+    ads = ads.all()
+
+    paginator = Paginator(ads, settings.ADS_LIST_ON_PAGE)
+
+    return render(request, 'index.html',
+                  {
+                      'ads': paginator.get_page(page),
+                      'paginator': paginator,
+                      'current_page': page,
+                      'current_category': category_selected,
+                      'search': search
+                  })
 
 
 def ad_view(request, id):
-
-    current_ad = Ad.objects.get(pk=id)
+    try:
+        current_ad = Ad.objects.get(pk=id)
+    except Ad.DoesNotExist:
+        return HttpResponse(status=404)
 
     return render(request, 'ad_single.html', {'current_ad': current_ad})
--- ad_buy/ad_buy/settings.py
+++ ad_buy/ad_buy/settings.py
@@ -127,3 +127,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+
+ADS_LIST_ON_PAGE = 25
